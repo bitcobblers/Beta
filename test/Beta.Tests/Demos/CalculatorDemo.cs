a01fdf10cfbae152bc4e1a1cc3680be2815ee934
Michael Vastarelli <michael.vastarelli@gmail.com>
2023-07-15 21:10:18 -0400

    Combined test classes.

--- test/Beta.Tests/Demos/CalculatorDemo.cs
+++ test/Beta.Tests/Demos/CalculatorDemo.cs
@@ -1,80 +1,12 @@
-﻿using System.Linq.Expressions;
-using FluentAssertions;
+﻿using FluentAssertions;
 
 namespace Beta.Tests.Demos;
 
-public class CalculatorDemoGuided : GuidedTest
-{
-    public CalculatorDemoGuided()
-    {
-        Test("Addition Tests (guided)", AdditionInput, (test, input) =>
-        {
-            test
-                .Arrange(() => new Calculator())
-                .Act(calc => calc.Add(input.A, input.B))
-                .Assert(result =>
-                {
-                    result.Should().Be(input.Expected);
-                });
-        });
-    }
-
-    private static IEnumerable<Input> AdditionInput
-    {
-        get
-        {
-            return new Input[]
-            {
-                new(1, 2, 3),
-                new(2, 3, 5)
-            };
-        }
-    }
-
-    private record Input(int A, int B, int Expected);
-
-    public class Calculator
-    {
-        public int Add(int a, int b) => a + b;
-    }
-}
-
-public class CalculatorDemo : BasicTest
+public class CalculatorDemo : BetaTest
 {
     public CalculatorDemo()
     {
-        // Guided test.
-        //AdditionInput.ToTest(this, "Addition Tests (Guided)", (test, input) =>
-        //{
-        //    test
-        //        .AddSteps(steps => steps
-        //        .Arrange(() => new Calculator())
-        //        .Act(calc => calc.Add(input.A, input.B))
-        //        .Assert(result =>
-        //        {
-        //            result.Should().Be(input.Expected);
-        //        })
-        //    );
-        //});
-
-        //// Basic (escape hatch) test.
-        //AdditionInput.ToTest(this, "Addition Tests (Basic)", (test, input) =>
-        //{
-        //    test
-        //        .Basic(() =>
-        //        {
-        //            // Arrange.
-        //            var calculator = new Calculator();
-
-        //            // Act.
-        //            int result = calculator.Add(input.A, input.B);
-
-        //            // Assert.
-        //            result.Should().Be(input.Expected);
-        //        });
-        //});
-
-        Test("Addition Tests (basic)", AdditionInput, input =>
+        BasicTest("Addition Tests (basic)", AdditionInput, input =>
         {
             // Arrange.
             var calculator = new Calculator();
@@ -84,6 +16,17 @@ public class CalculatorDemo : BasicTest
 
             // Assert.
             result.Should().Be(input.Expected);
+        });
+
+        GuidedTest("Addition Tests (guided)", AdditionInput, (test, input) =>
+        {
+            test
+                .Arrange(() => new Calculator())
+                .Act(calc => calc.Add(input.A, input.B))
+                .Assert(result =>
+                {
+                    result.Should().Be(input.Expected);
+                });
         });
     }
 
