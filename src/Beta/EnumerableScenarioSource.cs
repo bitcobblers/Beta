6af199e50a0bf9661e9b3d1dd413c2a78c32a358
Michael Vastarelli <mike@bitcobblers.com>
2024-03-02 09:56:03 -0500

    chore: cleaned up imports.

--- src/Beta/EnumerableScenarioSource.cs
+++ src/Beta/EnumerableScenarioSource.cs
@@ -2,9 +2,20 @@ using System.Collections;
 
 namespace Beta;
 
+[PublicAPI]
+#pragma warning disable CS9113 // Parameter is unread.
 public class EnumerableScenarioSource<T>(IEnumerable<T> scenarios) : IScenarioSource<T>
+#pragma warning restore CS9113 // Parameter is unread.
 {
     public bool SupportsEagerLoading => true;
-    public IEnumerator<T> GetEnumerator() => scenarios.GetEnumerator();
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
