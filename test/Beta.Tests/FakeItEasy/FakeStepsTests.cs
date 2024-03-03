bebfb4d383126d1576d2ce1669b5db24f58eedc3
Michael Vastarelli <mike@bitcobblers.com>
2024-03-03 13:13:23 -0500

    feat: Added FakeItEasy support.

---
+++ test/Beta.Tests/FakeItEasy/FakeStepsTests.cs
@@ -0,0 +1,39 @@
+﻿using Beta.FakeItEasy;
+
+namespace Beta.Tests.FakeItEasy;
+
+using static FakeSteps;
+
+public class FakeStepsTests
+{
+    [Fact]
+    public async Task CanCreateFakeWithExtensionMethod()
+    {
+        // Arrange.
+        var proof = new TestContainerWithFake().TestWithFake();
+
+        // Act.
+        var result = await proof.Test().ToListAsync();
+
+        // Assert.
+        result.ShouldHaveSingleItem();
+        result[0].Success.ShouldBeTrue();
+    }
+
+    private class TestContainerWithFake : TestContainer
+    {
+        public Proof<string> TestWithFake()
+        {
+            return
+                from service in Fake<IStubService>(fake => A.CallTo(() => fake.DoWork()).Returns("expected"))
+                let result = Apply(service.DoWork)
+                select result.Assert(f => new ProofResult(f, f == "expected", string.Empty));
+        }
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public interface IStubService
+    {
+        string DoWork();
+    }
+}
---
+++ test/Beta.Tests/ProofTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Beta.Tests;
+
+public class ProofTests
+{
+    [Fact]
+    public async Task SinglePositiveAssertionReturnsSuccessResult()
+    {
+        // Arrange.
+        var proof = new Proof<bool>(true);
+        proof.Assert(actual => new ProofResult(actual, true, "It's true"));
+
+        // Act.
+        var results = await proof.Test().ToListAsync();
+
+        // Assert.
+        results.ShouldHaveSingleItem();
+        results[0].ShouldBe(new ProofResult(true, true, "It's true"));
+    }
+}
