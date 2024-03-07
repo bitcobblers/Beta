1cb426f4e148a2496604277e2f3c60cb92301ec4
Michael Vastarelli <mike@bitcobblers.com>
2024-03-07 17:29:15 -0500

    chore: added wrapper for DiaSession.

---
+++ src/Beta.Runner.TestAdapter/DiaSessionWrapper.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Navigation;
+
+namespace Beta.Runner.TestAdapter;
+
+public class DiaSessionWrapper : IDisposable
+{
+    private readonly DiaSession? _session;
+    private bool _disposed;
+
+    public DiaSessionWrapper(string assemblyFilePath)
+    {
+        try
+        {
+            _session = new DiaSession(assemblyFilePath);
+        }
+        catch
+        {
+            // ignored -- all resolutions will return null.
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~DiaSessionWrapper()
+    {
+        Dispose(false);
+    }
+
+    public INavigationData? GetNavigationData(MethodBase? method)
+    {
+        if (method?.DeclaringType?.FullName == null || _session == null)
+        {
+            return null;
+        }
+
+        var attr = method.GetCustomAttribute<AsyncStateMachineAttribute>()?.StateMachineType;
+
+        return attr is { FullName: not null } ?
+            _session.GetNavigationDataForMethod(attr.FullName, "MoveNext") :
+            _session.GetNavigationDataForMethod(method.DeclaringType.FullName, method.Name);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing && !_disposed)
+        {
+            _session?.Dispose();
+        }
+
+        _disposed = true;
+    }
+}
