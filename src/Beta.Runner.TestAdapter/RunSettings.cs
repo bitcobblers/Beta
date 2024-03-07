1170490460502d068cb44ae2c982e83a953c9022
Michael Vastarelli <mike@bitcobblers.com>
2024-03-07 17:32:57 -0500

    refactor: simplified code to only consider default parameters.

--- src/Beta.Runner.TestAdapter/RunSettings.cs
+++ src/Beta.Runner.TestAdapter/RunSettings.cs
@@ -1,31 +1,17 @@
-using System.Runtime.CompilerServices;
 using System.Xml.Linq;
 
 namespace Beta.Runner.TestAdapter;
 
 public class RunSettings
 {
-    public enum AppDomainSupport
-    {
-        IfAvailable = 1,
-        Denied = 3
-    }
-
-    public string SolutionDirectory { get; private set; }
-    public string ResultsDirectory { get; private set; }
-    public AppDomainSupport? AppDomain { get; private set; }
-    public bool CollectSourceInformation { get; private set; } = true;
-    public bool DisableSerialization { get; private set; } = false;
-    public bool? ParallelizeAssembly { get; private set; } = true;
-    public bool? ParallelizeTestCollections { get; private set; } = true;
+    public string? ResultsDirectory { get; private set; }
+    public string? SolutionDirectory { get; private set; }
+    public int MaxCpuCount { get; private set; }
+    public bool CollectSourceInformation { get; private set; }
     public string? TargetFrameworkVersion { get; private set; }
     public string? TargetPlatform { get; private set; }
+    public bool DesignMode { get; private set; }
     public int BatchSize { get; private set; }
-    public int MaxCpuCount { get; private set; }
-
-    public RunSettings()
-    {
-    }
 
     public static RunSettings Parse(string? xmlContent)
     {
@@ -47,7 +33,6 @@ public class RunSettings
             return settings;
         }
 
-
         var configElement = parsed.Element("RunConfiguration");
 
         if (configElement == null)
@@ -55,53 +40,30 @@ public class RunSettings
             return settings;
         }
 
-        settings.SolutionDirectory = ParseString(configElement, "SolutionDirectory") ?? string.Empty;
-        settings.ResultsDirectory = ParseString(configElement, "ResultsDirectory") ?? string.Empty;
-        settings.CollectSourceInformation = ParseNonNullableBool(configElement, "CollectSourceInformation");
-        settings.DisableSerialization = !ParseNonNullableBool(configElement, "DesignMode");
-        settings.AppDomain = ParseAppDomainSupport(configElement, "DisableAppDomain");
-        settings.ParallelizeAssembly = !ParseNullableBool(configElement, "DisableParallelization") ?? false;
-        settings.ParallelizeTestCollections = !ParseNullableBool(configElement, "DisableParallelization") ?? false;
-        settings.TargetFrameworkVersion = ParseString(configElement, "TargetFrameworkVersion");
-        settings.TargetPlatform = ParseString(configElement, "TargetPlatform");
-        settings.BatchSize = ParseNonNullableInt(configElement, "BatchSize");
-        settings.MaxCpuCount = ParseNonNullableInt(configElement, "MaxCpuCount");
+        settings.SolutionDirectory = ParseString(configElement, nameof(SolutionDirectory));
+        settings.ResultsDirectory = ParseString(configElement, nameof(ResultsDirectory));
+        settings.CollectSourceInformation = ParseBool(configElement, nameof(CollectSourceInformation));
+        settings.TargetFrameworkVersion = ParseString(configElement, nameof(TargetFrameworkVersion));
+        settings.TargetPlatform = ParseString(configElement, nameof(TargetPlatform));
+        settings.BatchSize = ParseInt(configElement, nameof(BatchSize));
+        settings.DesignMode = ParseBool(configElement, nameof(DesignMode));
+        settings.MaxCpuCount = ParseInt(configElement, nameof(MaxCpuCount));
 
         return settings;
     }
 
-    private static AppDomainSupport? ParseAppDomainSupport(XContainer xml, string name)
+    private static bool ParseBool(XContainer xml, string name, bool defaultValue = false)
     {
-        return ParseNullableBool(xml, name) switch
-        {
-            true => AppDomainSupport.Denied,
-            false => AppDomainSupport.IfAvailable,
-            _ => null
-        };
+        return bool.TryParse(xml.Element(name)?.Value, out var value) ? value : defaultValue;
     }
 
-    private static bool? ParseNullableBool(XContainer xml, string name)
+    private static int ParseInt(XContainer xml, string name, int defaultValue = 0)
     {
-        if (bool.TryParse(xml.Element(name)?.Value, out var value))
-        {
-            return value;
-        }
-
-        return null;
+        return int.TryParse(xml.Element(name)?.Value, out var value) ? value : defaultValue;
     }
 
-    private static bool ParseNonNullableBool(XContainer xml, string name)
+    private static string ParseString(XContainer xml, string name, string defaultValue = "")
     {
-        return bool.TryParse(xml.Element(name)?.Value, out var value) && value;
-    }
-
-    private static int ParseNonNullableInt(XContainer xml, string name)
-    {
-        return int.TryParse(xml.Element(name)?.Value, out var value) ? value : 1;
-    }
-
-    private static string? ParseString(XContainer xml, string name)
-    {
-        return xml.Element(name)?.Value;
+        return xml.Element(name)?.Value ?? defaultValue;
     }
 }
