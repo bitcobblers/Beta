2743bb959f8da7dde1949844562e813523402be2
Michael Vastarelli <mike@bitcobblers.com>
2024-03-07 17:50:25 -0500

    chore: dropped Prove() method.

--- src/Beta/BetaTest.cs
+++ src/Beta/BetaTest.cs
@@ -8,9 +8,4 @@ public record BetaTest(object Instance, string Name, Proof Proof)
     public Guid Id { get; } = Guid.NewGuid();
 
     public MethodBase? Method { get; set; }
-
-    public Task Prove()
-    {
-        return Task.CompletedTask;
-    }
 }
