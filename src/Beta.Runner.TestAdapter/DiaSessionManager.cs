975fc4e9165a2e0321d4e63d80f66540711e1cca
Michael Vastarelli <mike@bitcobblers.com>
2024-03-07 22:36:27 -0500

    refactor: Created manager for DiaSession wrapper.

---
+++ src/Beta.Runner.TestAdapter/DiaSessionManager.cs
@@ -0,0 +1,47 @@
+﻿namespace Beta.Runner.TestAdapter;
+
+public class DiaSessionManager : IDisposable
+{
+    private readonly Dictionary<string, DiaSessionWrapper> _sessions = new();
+
+    private bool _disposed;
+
+    ~DiaSessionManager()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public DiaSessionWrapper GetSession(string assemblyFilePath)
+    {
+        if (_sessions.TryGetValue(assemblyFilePath, out var session))
+        {
+            return session;
+        }
+
+        session = new DiaSessionWrapper(assemblyFilePath);
+        _sessions[assemblyFilePath] = session;
+
+        return session;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing && !_disposed)
+        {
+            foreach (var session in _sessions.Values)
+            {
+                session.Dispose();
+            }
+
+            _sessions.Clear();
+        }
+
+        _disposed = true;
+    }
+}
