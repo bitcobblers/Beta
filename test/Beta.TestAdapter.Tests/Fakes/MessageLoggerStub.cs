03a9969fb64f60d2657207b6dae93a5defcf6949
Michael Vastarelli <mike@bitcobblers.com>
2024-03-10 23:31:05 -0400

    chore: more linting issues.

--- test/Beta.TestAdapter.Tests/Fakes/MessageLoggerStub.cs
+++ test/Beta.TestAdapter.Tests/Fakes/MessageLoggerStub.cs
@@ -4,23 +4,23 @@ namespace Beta.TestAdapter.Tests.Fakes;
 
 public class MessageLoggerStub : IMessageLogger
 {
-    private readonly List<Tuple<TestMessageLevel, string>> messages = [];
+    private readonly List<Tuple<TestMessageLevel, string>> _messages = [];
 
-    public TestMessageLevel LatestTestMessageLevel => messages.Last().Item1;
-    public string LatestMessage => messages.Last().Item2;
+    public TestMessageLevel LatestTestMessageLevel => _messages.Last().Item1;
+    public string LatestMessage => _messages.Last().Item2;
 
-    public int Count => messages.Count;
+    public int Count => _messages.Count;
 
-    public IEnumerable<Tuple<TestMessageLevel, string>> Messages => messages;
+    public IEnumerable<Tuple<TestMessageLevel, string>> Messages => _messages;
 
     public IEnumerable<Tuple<TestMessageLevel, string>> WarningMessages =>
-        messages.Where(o => o.Item1 == TestMessageLevel.Warning);
+        _messages.Where(o => o.Item1 == TestMessageLevel.Warning);
 
     public IEnumerable<Tuple<TestMessageLevel, string>> ErrorMessages =>
-        messages.Where(o => o.Item1 == TestMessageLevel.Error);
+        _messages.Where(o => o.Item1 == TestMessageLevel.Error);
 
     public void SendMessage(TestMessageLevel testMessageLevel, string message)
     {
-        messages.Add(new Tuple<TestMessageLevel, string>(testMessageLevel, message));
+        _messages.Add(new Tuple<TestMessageLevel, string>(testMessageLevel, message));
     }
 }
