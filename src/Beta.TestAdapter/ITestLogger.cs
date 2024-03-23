17cbc6a7b91594967b847fcfa626400474bffb8d
Michael Vastarelli <mike@bitcobblers.com>
2024-03-23 09:45:09 -0400

    test: added tests for logger.

--- src/Beta.TestAdapter/ITestLogger.cs
+++ src/Beta.TestAdapter/ITestLogger.cs
@@ -1,6 +1,17 @@
 ﻿namespace Beta.TestAdapter;
 
 /// <summary>
+///     Defines the various log levels.
+/// </summary>
+public enum LogLevel
+{
+    Debug = 0,
+    Info = 1,
+    Warn = 2,
+    Error = 3
+}
+
+/// <summary>
 ///     Defines an internal logger interface for use by the runner.
 /// </summary>
 public interface ITestLogger
@@ -11,42 +22,56 @@ public interface ITestLogger
     string Scope { get; }
 
     /// <summary>
+    ///     Writes a log message.
+    /// </summary>
+    /// <param name="level">The level of the message to write.</param>
+    /// <param name="message">The message to write.</param>
+    /// <param name="ex">An optional exception to include.</param>
+    void Log(LogLevel level, string message, Exception? ex = null);
+
+    /// <summary>
     ///     Logs a debug message.
     /// </summary>
     /// <param name="message">The message to log.</param>
-    void Debug(string message);
+    void Debug(string message) =>
+        Log(LogLevel.Debug, message);
 
     /// <summary>
     ///     Logs an info message.
     /// </summary>
     /// <param name="message">The message to log.</param>
-    void Info(string message);
+    void Info(string message) =>
+        Log(LogLevel.Info, message);
 
     /// <summary>
     ///     Logs a warning message.
     /// </summary>
     /// <param name="message">The message to log.</param>
-    void Warning(string message);
+    void Warning(string message) =>
+        Log(LogLevel.Warn, message);
 
     /// <summary>
     ///     Logs a warning message.
     /// </summary>
     /// <param name="message">The message to log.</param>
     /// <param name="ex">The exception to log.</param>
-    void Warning(string message, Exception? ex);
+    void Warning(string message, Exception? ex) =>
+        Log(LogLevel.Warn, message, ex);
 
     /// <summary>
     ///     Logs an error message.
     /// </summary>
     /// <param name="message">The message to log.</param>
-    void Error(string message);
+    void Error(string message) =>
+        Log(LogLevel.Error, message);
 
     /// <summary>
     ///     Logs an error message.
     /// </summary>
     /// <param name="message">The message to log.</param>
     /// <param name="ex">The exception to log.</param>
-    void Error(string message, Exception? ex);
+    void Error(string message, Exception? ex) =>
+        Log(LogLevel.Error, message, ex);
 
     /// <summary>
     ///     Creates a new logging scope.
--- src/Beta.TestAdapter/TestLogger.cs
+++ src/Beta.TestAdapter/TestLogger.cs
@@ -3,74 +3,57 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 
 namespace Beta.TestAdapter;
 
-public enum LogLevel
-{
-    Debug = 0,
-    Info = 1,
-    Warn = 2,
-    Error = 3
-}
-
-public class TestLogger(string scope, Stopwatch stopwatch, IMessageLogger? logger, int verbosity) :
-    ITestLogger, IMessageLogger
+/// <summary>
+///     Defines the default logger that forwards to the vstest logger.
+/// </summary>
+/// <param name="scope">The scope of the logger.</param>
+/// <param name="stopwatch">The stopwatch to track performance metrics.</param>
+/// <param name="logger">The underlying logger to write to.</param>
+/// <param name="verbosity">The verbosity of messages to write.</param>
+public class TestLogger(string scope, Stopwatch stopwatch, IMessageLogger? logger, int verbosity)
+    : ITestLogger, IMessageLogger
 {
     public TestLogger(IMessageLogger? logger, LogLevel verbosity = LogLevel.Debug)
         : this("/", Stopwatch.StartNew(), logger, (int)verbosity)
     {
     }
 
+    /// <summary>
+    ///     Gets a null logger that swallows all messages.
+    /// </summary>
     public static ITestLogger Null { get; } = new TestLogger(null, LogLevel.Error);
 
     /// <inheritdoc />
-    public void SendMessage(TestMessageLevel testMessageLevel, string message)
-    {
+    public void SendMessage(TestMessageLevel testMessageLevel, string message) =>
         logger?.SendMessage(testMessageLevel, message);
-    }
 
     /// <inheritdoc />
     public string Scope => scope;
 
     /// <inheritdoc />
-    public void Debug(string message) => Log(LogLevel.Debug, message);
-
-    /// <inheritdoc />
-    public void Info(string message) => Log(LogLevel.Info, message);
-
-    /// <inheritdoc />
-    public void Warning(string message) => Warning(message, null);
-
-    /// <inheritdoc />
-    public void Warning(string message, Exception? ex)
-    {
-        Log(LogLevel.Warn, message);
-
-        if (ex != null)
-        {
-            Log(LogLevel.Warn, ex.ToString());
-        }
-    }
-
-    /// <inheritdoc />
-    public void Error(string message) => Error(message, null);
-
-    /// <inheritdoc />
-    public void Error(string message, Exception? ex)
-    {
-        Log(LogLevel.Error, message);
-
-        if (ex != null)
-        {
-            Log(LogLevel.Error, ex.ToString());
-        }
-    }
-
-    /// <inheritdoc />
     public ITestLogger CreateScope(string newScope) =>
         new TestLogger(FormatScope(newScope), stopwatch, logger, verbosity);
 
-    internal static TestMessageLevel ToTestMessageLevel(LogLevel level)
+    /// <inheritdoc />
+    public void Log(LogLevel level, string message, Exception? ex = null)
     {
-        return level switch
+        if (!ShouldLog(level))
+        {
+            return;
+        }
+
+        var messageLevel = ToTestMessageLevel(level);
+
+        SendMessage(messageLevel, FormatMessage(message));
+
+        if (ex != null)
+        {
+            SendMessage(messageLevel, FormatMessage(ex.ToString()));
+        }
+    }
+
+    internal static TestMessageLevel ToTestMessageLevel(LogLevel level) =>
+        level switch
         {
             LogLevel.Debug => TestMessageLevel.Informational,
             LogLevel.Info => TestMessageLevel.Informational,
@@ -78,15 +61,6 @@ public class TestLogger(string scope, Stopwatch stopwatch, IMessageLogger? logge
             LogLevel.Error => TestMessageLevel.Error,
             _ => TestMessageLevel.Informational
         };
-    }
-
-    private void Log(LogLevel level, string message)
-    {
-        if (ShouldLog(level))
-        {
-            SendMessage(ToTestMessageLevel(level), FormatMessage(message));
-        }
-    }
 
     private string FormatMessage(string message)
     {
@@ -95,7 +69,9 @@ public class TestLogger(string scope, Stopwatch stopwatch, IMessageLogger? logge
         return scope == "/" ? $"[Beta {elapsed}] {message}" : $"[Beta {elapsed}] {scope}: {message}";
     }
 
-    private string FormatScope(string newScope) => scope == "/" ? $"/{newScope}" : $"{scope}/{newScope}";
+    private string FormatScope(string newScope) =>
+        scope == "/" ? $"/{newScope}" : $"{scope}/{newScope}";
 
-    private bool ShouldLog(LogLevel level) => (int)level >= verbosity;
+    internal bool ShouldLog(LogLevel level) =>
+        (int)level >= verbosity;
 }
