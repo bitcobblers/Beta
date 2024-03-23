983ad0d62c9d131bda9cf277a36cf9de4236d587
Michael Vastarelli <mike@bitcobblers.com>
2024-03-23 09:46:11 -0400

    test: marked to exclude from code coverage.

--- src/Beta.TestAdapter/NavigationData.cs
+++ src/Beta.TestAdapter/NavigationData.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Beta.TestAdapter;
 
 /// <summary>
-/// Defines the navigation data for a single test.
+///     Defines the navigation data for a single test.
 /// </summary>
 /// <param name="FileName">The file where the test is defined.</param>
 /// <param name="LineNumber">The line number where the test method is.</param>
+[ExcludeFromCodeCoverage]
 public record NavigationData(string FileName, int LineNumber);
