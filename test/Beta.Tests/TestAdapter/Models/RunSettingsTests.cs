c34d13a978aaea427bc379935c36027c2e4279a7
Michael Vastarelli <mike@bitcobblers.com>
2024-03-23 12:56:20 -0400

    test: added tests for RunSettings.

---
+++ test/Beta.Tests/TestAdapter/Models/RunSettingsTests.cs
@@ -0,0 +1,49 @@
+﻿using Beta.TestAdapter.Models;
+
+namespace Beta.Tests.TestAdapter.Models;
+
+public class RunSettingsTests
+{
+    public class SpaceParseMethod : RunSettingsTests
+    {
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [Theory]
+        public void ReturnsDefaultSettingsWhenXmlIsNullOrWhite(string? inputXml)
+        {
+            // Act.
+            var settings = RunSettings.Parse(inputXml);
+
+            // Assert.
+            Assert.NotNull(settings);
+            Assert.NotNull(settings.Configuration);
+        }
+
+        [Fact]
+        public void ReturnsDefaultRunConfigurationWhenMissingFromConfiguration()
+        {
+            // Arrange.
+            const string xml = "<RunSettings></RunSettings>";
+
+            // Act.
+            var settings = RunSettings.Parse(xml);
+
+            // Assert.
+            Assert.NotNull(settings.Configuration);
+        }
+
+        [Fact]
+        public void ReturnsDefaultRunConfigurationWhenGivenMalformedXml()
+        {
+            // Arrange.
+            const string xml = "<RunSettings></ invalid-xml<>></RunSettings>";
+
+            // Act.
+            var settings = RunSettings.Parse(xml);
+
+            // Assert.
+            Assert.NotNull(settings.Configuration);
+        }
+    }
+}
