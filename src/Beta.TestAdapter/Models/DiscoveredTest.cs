d2ba1daf45e6e5ad8e342618eba16dda678b7717
Michael Vastarelli <mike@bitcobblers.com>
2024-03-26 18:01:20 -0400

    chore: fixed linting issues.

--- src/Beta.TestAdapter/Models/DiscoveredTest.cs
+++ src/Beta.TestAdapter/Models/DiscoveredTest.cs
@@ -1,9 +1,29 @@
-﻿namespace Beta.TestAdapter.Models;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
+namespace Beta.TestAdapter.Models;
+
+/// <summary>
+///     Defines a single discovered test case.
+/// </summary>
 public record DiscoveredTest
 {
+    /// <summary>
+    ///     Gets the class name of the test.
+    /// </summary>
     public string ClassName { get; init; }
+
+    /// <summary>
+    ///     Gets the method name of the test.
+    /// </summary>
     public string MethodName { get; init; }
+
+    /// <summary>
+    ///     Gets the input to apply to the test case.
+    /// </summary>
     public string Input { get; init; }
+
+    /// <summary>
+    ///     Gets the friendly name of the test.
+    /// </summary>
     public string TestName { get; init; }
 }
