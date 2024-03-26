4d1906c2592c4e5a82341761a2d0997f257405f1
Michael Vastarelli <mike@bitcobblers.com>
2024-03-26 18:06:31 -0400

    chore: exclude DTO from code coverage.

--- src/Beta.TestAdapter/Models/DiscoveredTest.cs
+++ src/Beta.TestAdapter/Models/DiscoveredTest.cs
@@ -1,10 +1,13 @@
-﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+﻿using System.Diagnostics.CodeAnalysis;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace Beta.TestAdapter.Models;
 
 /// <summary>
 ///     Defines a single discovered test case.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public record DiscoveredTest
 {
     /// <summary>
