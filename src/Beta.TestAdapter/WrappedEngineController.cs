7598542b12322deab3e559beb865a2016b3582cd
Michael Vastarelli <mike@bitcobblers.com>
2024-03-26 17:58:12 -0400

    feat: added skeletal code to read tests from the proxy.

--- src/Beta.TestAdapter/WrappedEngineController.cs
+++ src/Beta.TestAdapter/WrappedEngineController.cs
@@ -1,4 +1,5 @@
-﻿using Beta.TestAdapter.Models;
+﻿using System.Text.Json;
+using Beta.TestAdapter.Models;
 
 #pragma warning disable CS8618
 
@@ -10,7 +11,18 @@ namespace Beta.TestAdapter;
 /// <param name="instance">The instance to wrap.</param>
 public class WrappedEngineController(object instance) : IEngineController
 {
+    private readonly Type _instanceType = instance.GetType();
+
     /// <inheritdoc />
     public IEnumerable<DiscoveredTest> Query() =>
-        Enumerable.Empty<DiscoveredTest>();
+        from test in Execute<IEnumerable<string>>("Query", []) ?? []
+        where test != null
+        select JsonSerializer.Deserialize<DiscoveredTest>(test);
+
+    private T? Execute<T>(string methodName, object?[] parameters)
+        where T : class
+    {
+        var method = _instanceType.GetMethod(methodName);
+        return method?.Invoke(instance, parameters) as T;
+    }
 }
