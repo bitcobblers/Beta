5573c81992e4c344f84f57fabd64a3a15e4fd534
Michael Vastarelli <mike@bitcobblers.com>
2024-04-01 17:09:47 -0400

    chore: updated ctor to drop listener parameter.

--- test/Beta.Tests/Demos/DemoTests.cs
+++ test/Beta.Tests/Demos/DemoTests.cs
@@ -17,7 +17,7 @@ public class DemoTests(ITestOutputHelper output)
         var discoverer = new DefaultTestDiscoverer(testCaseDiscoverer);
         var aggregator = new DefaultTestSuiteAggregator([discoverer]);
 
-        var runner = new DefaultTestRunner(logger, A.Fake<ITestListener>());
+        var runner = new DefaultTestRunner(logger);
 
         return runner.Run(
             aggregator.Aggregate([typeof(CalculatorDemo)]),
