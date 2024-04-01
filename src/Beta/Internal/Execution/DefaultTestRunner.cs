9e0d3547053ddacacd7f1ab8238c2d186695d0d4
Michael Vastarelli <mike@bitcobblers.com>
2024-04-01 17:07:37 -0400

    chore: deleted demo code.

--- src/Beta/Internal/Execution/DefaultTestRunner.cs
+++ src/Beta/Internal/Execution/DefaultTestRunner.cs
@@ -7,69 +7,11 @@ namespace Beta.Internal.Execution;
 ///     Defines the default test runner.
 /// </summary>
 /// <param name="logger">The internal logger to use.</param>
-/// <param name="listener">The test listener to use.</param>
-public class DefaultTestRunner : ITestRunner
+public class DefaultTestRunner(ILogger logger) : ITestRunner
 {
-    // private readonly ITestListener _listener;
-    private readonly ILogger _logger;
-
-    public DefaultTestRunner(ILogger logger)
-    {
-        //  _listener = listener;
-        _logger = logger;
-    }
-
     /// <inheritdoc />
-    public Task Run(IDictionary<Guid, Test> testDirectory,
-                    IEnumerable<Guid> tests,
-                    CancellationToken cancellationToken) => Task.CompletedTask;
-    // var suites =
-    //     from test in tests
-    //     group test by test.TestClassName
-    //     into g
-    //     let suite = new
-    //     {
-    //         Instance = g.Key,
-    //         Tests = (from t in g
-    //                  where filter.Filter(t)
-    //                  select t).ToArray()
-    //     }
-    //     where suite.Tests.Length > 0
-    //     select suite;
-    //
-    // return Parallel.ForEachAsync(suites, cancellationToken, async (suite, token) =>
-    // {
-    //     if (token.IsCancellationRequested)
-    //     {
-    //         return;
-    //     }
-    //
-    //     foreach (var test in suite.Tests)
-    //     {
-    //         test.Instance.PreProcessors.ForEach(p => p.Process(test.Instance));
-    //         var result = new BetaTestResult(test.Id);
-    //         var outcome = TestOutcome.Passed;
-    //
-    //         try
-    //         {
-    //             await foreach (var proofResult in test.Apply().Test(cancellationToken))
-    //             {
-    //                 result.Results.Add(proofResult);
-    //                 listener.OnUpdate(result.Id, proofResult);
-    //                 logger.Debug(proofResult.ToString());
-    //
-    //                 if (proofResult.Success == false)
-    //                 {
-    //                     outcome = TestOutcome.Failed;
-    //                 }
-    //             }
-    //
-    //             listener.OnFinish(test.Id, DateTime.UtcNow, outcome, string.Empty);
-    //         }
-    //         finally
-    //         {
-    //             test.Instance.PostProcessors.ForEach(p => p.Process(test.Instance));
-    //         }
-    //     }
-    // });
+    public Task Run(
+        IEnumerable<Test> tests,
+        ITestFilter filter,
+        CancellationToken cancellationToken) => Task.CompletedTask;
 }
